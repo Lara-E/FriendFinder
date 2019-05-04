9f8a4fe7dc8567d85b0a634e1cca4186463ef13b
Lara Eller <lareh_eller@yahoo.com>
2019-05-04 19:47:58 -0400

    I added the API routes to apiRoutes.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var htmlRoutes = require("./app/routing/htmlRoutes.js");
+var apiRoutes = require("./app/routing/apiRoutes.js");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 htmlRoutes(app);
+apiRoutes(app);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
