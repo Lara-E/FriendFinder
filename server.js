08bf0ebee79a146b09d53259fa7b91f8abec3c51
Lara Eller <lareh_eller@yahoo.com>
2019-05-04 18:12:30 -0400

    I set up the root route in the htmlRoutes.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require("express");
-
+var htmlRoutes = require("./app/routing/htmlRoutes.js");
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+htmlRoutes(app);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
