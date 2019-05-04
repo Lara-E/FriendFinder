37cb7f09e19afc51b132f2572f876631c6d45935
Lara Eller <lareh_eller@yahoo.com>
2019-05-04 19:12:20 -0400

    I added the friends objects

---
+++ app/data/friends.js
@@ -0,0 +1,99 @@
+var friends = [
+    {
+        name: "Spock",
+        photo: "../images/spock.jpg",
+        scores: [
+            2,
+            1,
+            3,
+            2,
+            5,
+            2,
+            4,
+            5,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Dr. 'Bones' McCoy",
+        photo: "../images/bones.jpg",
+        scores: [
+            3,
+            5,
+            4,
+            1,
+            3,
+            2,
+            2,
+            1,
+            5,
+            4
+        ]
+    },
+    {
+        name: "James T. Kirk",
+        photo: "../images/kirk.jpg",
+        scores: [
+            4,
+            5,
+            2,
+            2,
+            3,
+            1,
+            1,
+            4,
+            3,
+            2
+        ]
+    },
+    {
+        name: "Scotty",
+        photo: "../images/scotty.jpg",
+        scores: [
+            1,
+            2,
+            3,
+            5,
+            5,
+            1,
+            1,
+            3,
+            3,
+            4
+        ]
+    },
+    {
+        name: "Sulu",
+        photo: "../images/sulu.jpg",
+        scores: [
+            5,
+            4,
+            3,
+            5,
+            1,
+            2,
+            3,
+            5,
+            4,
+            4
+        ]
+    },
+    {
+        name: "Uhura",
+        photo: "../images/uhura.jpg",
+        scores: [
+            2,
+            5,
+            1,
+            1,
+            3,
+            2,
+            3,
+            1,
+            4,
+            3
+        ]
+    }
+];
+
