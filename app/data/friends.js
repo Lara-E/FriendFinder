2b41e92ff6d12541406b5ee301f017c9d0e849b1
Lara Eller <lareh_eller@yahoo.com>
2019-05-05 02:57:31 -0400

    I added the modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
         name: "Spock",
-        photo: "../images/spock.jpg",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQ1wKN1A2gcPYizvYQhrabEWwQN05oLNPhGd2g0mZO1tvgTzgk",
         scores: [
             2,
             1,
@@ -17,7 +17,7 @@ var friends = [
     },
     {
         name: "Dr. 'Bones' McCoy",
-        photo: "../images/bones.jpg",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv9AxDCNpCxtWc70wFAvI34HTeOzfknMgGsHBQQlrn4S41bILq",
         scores: [
             3,
             5,
@@ -33,7 +33,7 @@ var friends = [
     },
     {
         name: "James T. Kirk",
-        photo: "../images/kirk.jpg",
+        photo: "https://japantoday-asset.scdn3.secure.raxcdn.com/img/store/c4/fd/af42121ff402091772d3f4ae6cb3d0843e82/kirk.jpg",
         scores: [
             4,
             5,
@@ -49,7 +49,7 @@ var friends = [
     },
     {
         name: "Scotty",
-        photo: "../images/scotty.jpg",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-VUYm16Wk9hfOKgbCVxLZq9LhvhNi42TaERGLY99SrDtZyn1y",
         scores: [
             1,
             2,
@@ -65,7 +65,7 @@ var friends = [
     },
     {
         name: "Sulu",
-        photo: "../images/sulu.jpg",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgwWPLmGyW1aCGwlFOIiD6XTnX4u8P7TfwAY3eUzsrU6bODLY4",
         scores: [
             5,
             4,
@@ -81,7 +81,7 @@ var friends = [
     },
     {
         name: "Uhura",
-        photo: "../images/uhura.jpg",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROn6yesZ2KH17ZOWaXXm0yYnuxdG5cUqx3fe3ngS3R9ccyZs2z9Q",
         scores: [
             2,
             5,
