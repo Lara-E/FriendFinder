fbd2f8f96bd59587ec851e028bf9cd2209cc6f0e
Lara Eller <lareh_eller@yahoo.com>
2019-05-04 23:09:58 -0400

    I added the user input post route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,17 @@
 var friends = require("../data/friends.js");
 
+
 module.exports = function (app) {
-    app.get("/api/friends", function (req, res) {
+    app.get("/api/friends", function(req, res) {
         return res.json(friends);
     })
 
-    app.post("/api/friends", function (req, res) {
+    app.post("/api/friends", function(req, res) {
         var newFriend = req.body;
 
+        console.log(newFriend)
         friends.push(newFriend);
 
-        res.json(newFriend);
+        return res.json(newFriend);
     })
 };
--- server.js
+++ server.js
@@ -4,11 +4,12 @@ var apiRoutes = require("./app/routing/apiRoutes.js");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 htmlRoutes(app);
 apiRoutes(app);
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
